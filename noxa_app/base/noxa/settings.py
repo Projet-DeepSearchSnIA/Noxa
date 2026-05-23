f691685d255a31c4d146a7af6b69b84940fc2b73
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2026-05-23 22:39:41 +0000

    fix: db connection health check pour Neon serverless

--- noxa_app/base/noxa/settings.py
+++ noxa_app/base/noxa/settings.py
@@ -97,7 +97,8 @@ WSGI_APPLICATION = 'noxa.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL', ''),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True
     )
 }
 
